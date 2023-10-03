fc30cd08a40c24c627b1a7de713fac757e7c743d
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-10-03 16:26:13 +0530

    🔨 | added server & client

---
+++ javascript/src/client.js
@@ -0,0 +1,43 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import parseArgs from 'minimist';
+import { loadPackageDefinition, credentials } from '@grpc/grpc-js';
+import { loadSync } from '@grpc/proto-loader';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const PROTO_PATH =  __dirname + '/proto/greeter.proto';
+
+let packageDefinition = loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+let greeter_proto = loadPackageDefinition(packageDefinition).greeter;
+
+(() => {
+    let argv = parseArgs(process.argv.slice(2), {
+        string: 'target'
+    });
+    let target;
+    if (argv.target) {
+        target = argv.target;
+    } else {
+        target = 'localhost:50051';
+    }
+    let client = new greeter_proto.Greeter(target,
+        credentials.createInsecure());
+    let user;
+    if (argv._.length > 0) {
+        user = argv._[0];
+    } else {
+        user = 'World';
+    }
+    client.send({ name: user }, function (err, response) {
+        console.log('Greeting:', response.message);
+    });
+})();

f638e169609904cef490a93701ba8291edbd5f28
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-10-03 17:13:48 +0530

    🔨 | added '/src' to 'typescript'

---
+++ typescript/src/server.ts
@@ -0,0 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { loadPackageDefinition, Server, ServerCredentials } from "@grpc/grpc-js";
+import { loadSync } from "@grpc/proto-loader";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const PROTO_PATH = __dirname + '/proto/greeter.proto';
+
+const packageDefinition = loadSync(PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true })
+const greeter_proto = loadPackageDefinition(packageDefinition).greeter;
+
+function sendMessage(call: any, callback: any) {
+  console.log(`${call.request.name} has called`);
+  callback(null, { message: 'Hello ' + call.request.name });
+}
+
+(() => {
+  const server = new Server();
+  server.addService(greeter_proto.Greeter.service, { Send: sendMessage });
+  server.bindAsync('0.0.0.0:50051', ServerCredentials.createInsecure(), () => {
+    server.start();
+    console.log('Server listening on port 50051...');
+  })
+})();
